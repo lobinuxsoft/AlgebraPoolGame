7345439222973795f82eabb155d33c20fa9d6698
Ramiro Guzman <rami.guzmi@gmail.com>
2021-11-04 11:06:56 -0300

    Movmiento fase 1
    Se esta intentando aplicar movimiento teniendo en cuentas la fisica y las fuerzas que afecta la velocidad del objeto

--- Assets/Script/PhysicBody.cs
+++ Assets/Script/PhysicBody.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PhysicBody : MonoBehaviour
 {
@@ -7,20 +7,49 @@ public class PhysicBody : MonoBehaviour
         Circle, Box
     }
 
-    [SerializeField] ShapeType shapeType = ShapeType.Circle;
-    [SerializeField] private Vector3 linearVelocity = Vector3.zero;
-    [SerializeField] float rotation = 0;
-    [SerializeField] float rotationalVelocity = 0;
+    //[SerializeField] ShapeType shapeType = ShapeType.Circle;
+    //[SerializeField] private Vector3 linearVelocity = Vector3.zero;
+    //[SerializeField] float rotation = 0;
+    //[SerializeField] float rotationalVelocity = 0;
 
-    [SerializeField] float density = 0;
-    [SerializeField] float mass = 0;
-    [SerializeField] float restitution = 0;
-    [SerializeField] float area;
+    //[SerializeField] float density = 0;
+    //[SerializeField] float restitution = 0;
+    //[SerializeField] float area;
+    //  [SerializeField] bool isStatic = false;
 
-    [SerializeField] bool isStatic = false;
+    //float radius = 0;
+    //float width = 0;    
+    //float height = 0;
 
-    float radius = 0;
-    float width = 0;
-    float height = 0;
+    [SerializeField] Vector3 direction;
+    [SerializeField] float velocity;
+    [SerializeField] float aceleration;
+    [SerializeField] float mass = 5.0f;
+    [SerializeField] float force = 15.0f;
 
+    [SerializeField] float gravity = -9.8f;
+
+    private void Update()
+    {
+
+        aceleration = force / mass;
+        aceleration -= gravity * Time.deltaTime;
+        velocity += aceleration;
+
+        transform.position += direction * velocity;
+    }
+    void addForce()
+    {
+
+    }
+
+    //ACELERACION : a = Δv / Δt;
+    //ACELERACION : a = F/m;
+
+    //MRUV : d = 1/2.a.t2;
+     
+    //FUERZA : m * a;
+
+    
 }
+
