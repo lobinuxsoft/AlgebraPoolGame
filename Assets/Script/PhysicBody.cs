6b6216a1ea6b4689c9a9e5e89935db1d530482cd
Lobinux <lobinux@digilearnnials.com>
2021-11-04 22:55:12 -0300

    Parte gráfica de las colisiones lista

--- Assets/Script/PhysicBody.cs
+++ Assets/Script/PhysicBody.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public class PhysicBody : MonoBehaviour
@@ -8,19 +9,58 @@ public class PhysicBody : MonoBehaviour
     }
 
     [SerializeField] ShapeType shapeType = ShapeType.Circle;
+    [SerializeField] private bool isStatic = false;
+    [SerializeField] private float radius = 0;
+    [SerializeField] private float width = 0;
+    [SerializeField] private float height = 0;
+
     [SerializeField] private Vector3 linearVelocity = Vector3.zero;
-    [SerializeField] float rotation = 0;
-    [SerializeField] float rotationalVelocity = 0;
+    [SerializeField] private float rotation = 0;
+    [SerializeField] private float rotationalVelocity = 0;
 
-    [SerializeField] float density = 0;
-    [SerializeField] float mass = 0;
-    [SerializeField] float restitution = 0;
-    [SerializeField] float area;
+    [SerializeField] private float density = 0;
+    [SerializeField] private float mass = 0;
+    [SerializeField, Range(0,1)] private float restitution = 0; // Esto es con la potencia que rebota.
+    
+    private float area = 0;
 
-    [SerializeField] bool isStatic = false;
+   
 
-    float radius = 0;
-    float width = 0;
-    float height = 0;
+    private void Start()
+    {
+        linearVelocity = Vector3.zero;
+        rotation = 0;
+        rotationalVelocity = 0;
+        restitution = Mathf.Clamp01(restitution);
 
+        switch (shapeType)
+        {
+            case ShapeType.Circle:
+                area = radius * radius * Mathf.PI;
+                break;
+            case ShapeType.Box:
+                area = width * height;
+                break;
+        }
+
+        mass = area * density;
+    }
+
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        switch (shapeType)
+        {
+            case ShapeType.Circle:
+                Handles.color = Color.green;
+                Handles.DrawWireDisc(transform.position, Vector3.forward, radius/2, 1f);
+                Handles.DrawLine(transform.position, transform.right * radius / 2);
+                break;
+            case ShapeType.Box:
+                Handles.DrawSolidRectangleWithOutline(new Rect(transform.position.x - width / 2, transform.position.y - height / 2, width, height), Color.clear, Color.green);
+                break;
+        }
+    }
+#endif
 }
--- Assets/Script/PhysicsWorld.cs
+++ Assets/Script/PhysicsWorld.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class PhysicsWorld : MonoBehaviour
 {
-    [SerializeField] private List<PhysicBody> physicBodies = new List<PhysicBody>();
+    [SerializeField] private PhysicBody[] physicBodies;
     [SerializeField] private FlatVector flatVector = new FlatVector(3,2);
+
+    private void Start()
+    {
+        physicBodies = FindObjectsOfType<PhysicBody>();
+    }
 
     private void Update()
     {
-        if(physicBodies.Count > 0)
+        if(physicBodies.Length > 0)
         {
 
         }
-    }
-
-    void AddObject(PhysicBody physicBody)
-    {
-        physicBodies.Add(physicBody);
     }
 
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
         FlatVector flatNormalized = FlatMath.Normalize(flatVector);
-        Handles.color = Color.yellow;
+        Handles.color = Mathf.CorrelatedColorTemperatureToRGB(FlatMath.Magnitude(flatVector));
         Handles.ArrowHandleCap(0, Vector3.zero, Quaternion.LookRotation(new Vector3(flatVector.x, flatVector.y)), FlatMath.Magnitude(flatVector), EventType.Repaint);
         Handles.DrawWireDisc(Vector3.zero, Vector3.forward, FlatMath.Magnitude(flatVector));
         Handles.color = Color.blue;
