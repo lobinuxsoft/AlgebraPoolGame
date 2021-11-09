ace1443c7a2fc651144751459d399a50bb8872bc
Ramiro Guzman <rami.guzmi@gmail.com>
2021-11-09 11:58:38 -0300

    Prototipo de calculo de rozamiento con el aire
    Aun no funciona, es un modelo preliminar pero es la base de lo que proximamente va a ser la reistencia aerodinamica

--- Assets/Script/PhysicBody.cs
+++ Assets/Script/PhysicBody.cs
@@ -27,20 +27,23 @@ public class PhysicBody : MonoBehaviour
     [SerializeField] float mass = 5.0f;
     [SerializeField] float force = 15.0f;
 
-
     [SerializeField] float coefficientFriction = 0.0f; // 0.18 - 0.24
     [SerializeField] float tableFriction = 0.2f; // 0.18 - 0.24
 
     [SerializeField] float gravity = 9.8f;
 
+    Vector3 coeficienteRozamientoAire = Vector3.zero;
+    float CONST_ROZAMIENTO_AIRE = 0.0000000000667f;
     private void Start()
     {
         aceleration = force / mass;
         coefficientFriction = tableFriction * (mass * gravity);
     }
+
     private void Update()
     {
         aceleration -= coefficientFriction * Time.deltaTime;
+        //aceleration -= coeficienteRozamientoAire * Time.deltaTime;
 
         if (aceleration < 0)
         {
@@ -49,6 +52,9 @@ public class PhysicBody : MonoBehaviour
 
         velocity = direction * aceleration * Time.deltaTime;
 
+        coeficienteRozamientoAire = velocityNormalizeSquared(velocity) * CONST_ROZAMIENTO_AIRE * -1;      //Es una prueba desprolija xd 
+        velocity -= coeficienteRozamientoAire * Time.deltaTime;
+
         transform.position += velocity;
 
     }
@@ -56,6 +62,17 @@ public class PhysicBody : MonoBehaviour
     {
 
     }
+    Vector3 velocityNormalizeSquared(Vector3 a)
+    {
+        a = a.normalized;
+
+        a.x *= a.x;
+        a.y *= a.y;
+        a.z *= a.z;
+
+        return a; //Normaliza el vector y lo devuevle al cuadrado
+    }
+    //Constante de resistencia aerodnamica =  0,0000000000667
 
     //ACELERACION : a = Δv / Δt;
     //ACELERACION : a = F/m;
@@ -66,5 +83,7 @@ public class PhysicBody : MonoBehaviour
 
     //Coeficiente de rozamiente: Fr = μ * N (Rozamiento entre 2 superficies = coeficiente de rozamiento (mesa) * Fuerza Normal)
 
+    //Formula de rozamiento con el aire:          Fdrag = ||v||2 * Cd * -1
+    //Fuerza de arrastre = velocidad.normalize² * Coeficiente de friccion * -1 (El signo indica el lado al que se aplica la fuerza)
 }
 
