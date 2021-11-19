c6b971cccfc4efaa35216dcc25a0913eb49ac426
facundo <facuwilli@gmail.com>
2021-11-19 10:15:23 -0300

    Charge forces with input.

--- Assets/Script/HitWhiteBall.cs
+++ Assets/Script/HitWhiteBall.cs
@@ -8,14 +8,14 @@ public class HitWhiteBall : MonoBehaviour
     [SerializeField] float forceMultiplier = 30.5f;  
 
     private PhysicBody whiteBall;
-    private LineRenderer ballMouseLine;
-    private Gradient lineColor = new Gradient();
+    private LineRenderer ballMouseLine;    
     private Camera cam;    
     private Vector2 pixelCoordinatesMousePos;
     private Vector3 worldCoordinatesMousePos;
-    private Vector3 direction = new Vector3(0.0f, 0.0f, 0.0f);    
-    private float distanceMouseBall;     
-    private float force = 0;   
+    private Vector3 direction = new Vector3(0.0f, 0.0f, 0.0f);
+    private Vector3 positionWhereMousePressed;
+    private float distanceMousePressedMouseReleased = 0.0f;    
+    private float force = 0.0f;   
 
     void Start()
     {
@@ -23,29 +23,39 @@ public class HitWhiteBall : MonoBehaviour
         whiteBall = GetComponent<PhysicBody>();
         ballMouseLine = GetComponent<LineRenderer>();       
         ballMouseLine.positionCount = 2;
-        ballMouseLine.startWidth = 0.25f;
-        ballMouseLine.endWidth = 0.25f;
+        ballMouseLine.startWidth = 0.15f;
+        ballMouseLine.endWidth = 0.15f;
+        ballMouseLine.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         pixelCoordinatesMousePos = Input.mousePosition;
-        worldCoordinatesMousePos = cam.ScreenToWorldPoint(new Vector3(pixelCoordinatesMousePos.x, pixelCoordinatesMousePos.y, cam.nearClipPlane)); //Sacas la coordenada en wordl coordinates              
 
-        ballMouseLine.SetPosition(0, worldCoordinatesMousePos);
-        ballMouseLine.SetPosition(1, whiteBall.transform.position);       
+        worldCoordinatesMousePos = cam.ScreenToWorldPoint(new Vector3(pixelCoordinatesMousePos.x, pixelCoordinatesMousePos.y, cam.nearClipPlane)); //Sacas la posicion del mouse en coordenadas de mundo         
+
+        distanceMousePressedMouseReleased = Vector2.Distance(worldCoordinatesMousePos, positionWhereMousePressed); //Sacamos distancia entre donde se preciono y donde se libero el click del mouse
+        distanceMousePressedMouseReleased /= maxDistanceForce;
+
+        direction = (positionWhereMousePressed - worldCoordinatesMousePos).normalized; //Sacas la direccion normalizada del vector
+
+        ballMouseLine.SetPosition(0, whiteBall.transform.position - (direction * distanceMousePressedMouseReleased));
+        ballMouseLine.SetPosition(1, whiteBall.transform.position);
+
+        if (Input.GetMouseButtonDown(0)) 
+        {            
+            ballMouseLine.enabled = true;
+            positionWhereMousePressed = worldCoordinatesMousePos;
+        }
 
         if (Input.GetMouseButtonUp(0)) 
-        {
-            distanceMouseBall = Vector2.Distance(worldCoordinatesMousePos, whiteBall.transform.position); //Sacamos distancia entre mouse y la pelota
+        {            
+            ballMouseLine.enabled = false;
+            
+            force = distanceMousePressedMouseReleased * forceMultiplier; //Calculamos la fuerza que va a resivir el rigid body
 
-            direction = (whiteBall.transform.position - worldCoordinatesMousePos).normalized; //Sacas la direccion normalizada del vector 
-
-            force = (distanceMouseBall / maxDistanceForce) * forceMultiplier; //Calculamos la fuerza que va a resivir el rigid body
-
-            whiteBall.HitByCue(direction, force);
+            whiteBall.HitByCue(direction, force); //Le mandamos la direccion y la fuerza al rigid body
         }        
     }    
 }
