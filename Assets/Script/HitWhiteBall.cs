af06d548b4b073ad278f0d33f7ee19229e80d3fa
facundo <facuwilli@gmail.com>
2021-11-25 09:58:06 -0300

    Finished hit white ball.

--- Assets/Script/HitWhiteBall.cs
+++ Assets/Script/HitWhiteBall.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class HitWhiteBall : MonoBehaviour
 {
-    [SerializeField] float maxDistanceForce = 3.5f;
+    [SerializeField] float maxDistanceForce = 1.2f;
     [SerializeField] float forceMultiplier = 30.5f;  
 
     private PhysicBody whiteBall;
@@ -14,7 +14,7 @@ public class HitWhiteBall : MonoBehaviour
     private Vector2 pixelCoordinatesMousePos;
     private Vector3 worldCoordinatesMousePos;
     private Vector3 direction = new Vector3(0.0f, 0.0f, 0.0f);
-    private Vector3 positionWhereMousePressed;
+    private Vector3 positionWhereMousePressed = new Vector3(0.0f, 0.0f, 0.0f);
     private float distanceMousePressedMouseReleased = 0.0f;    
     private float force = 0.0f;   
 
@@ -24,8 +24,8 @@ public class HitWhiteBall : MonoBehaviour
         whiteBall = GetComponent<PhysicBody>();
         ballMouseLine = GetComponent<LineRenderer>();       
         ballMouseLine.positionCount = 2;
-        ballMouseLine.startWidth = 0.15f;
-        ballMouseLine.endWidth = 0.15f;
+        ballMouseLine.startWidth = 1.0f;
+        ballMouseLine.endWidth = 1.0f;
         ballMouseLine.enabled = false;
     }
 
@@ -37,12 +37,12 @@ public class HitWhiteBall : MonoBehaviour
         worldCoordinatesMousePos = cam.ScreenToWorldPoint(new Vector3(pixelCoordinatesMousePos.x, pixelCoordinatesMousePos.y, cam.nearClipPlane)); //Sacas la posicion del mouse en coordenadas de mundo         
         
         distanceMousePressedMouseReleased = Vector2.Distance(worldCoordinatesMousePos, positionWhereMousePressed); //Sacamos distancia entre donde se preciono y donde se libero el click del mouse
-        distanceMousePressedMouseReleased /= maxDistanceForce; //Te aseguras que la distancia no sea mayor a un maximo
-
+        distanceMousePressedMouseReleased = Mathf.Clamp(distanceMousePressedMouseReleased, 0.0f, maxDistanceForce); //Te aseguras que la distancia no sea mayor a un maximo
+                
         direction = (positionWhereMousePressed - worldCoordinatesMousePos).normalized; //Sacas la direccion normalizada del vector
-                                                                                       //
-        ballMouseLine.SetPosition(0, whiteBall.transform.position - (direction * distanceMousePressedMouseReleased));
-        ballMouseLine.SetPosition(1, whiteBall.transform.position);
+                                                                                       
+        ballMouseLine.SetPosition(0, whiteBall.transform.position - (direction * distanceMousePressedMouseReleased)); //Posición final de la flecha
+        ballMouseLine.SetPosition(1, whiteBall.transform.position); //Posicion inicial de la flecha
 
         if (Input.GetMouseButtonDown(0)) 
         {            
@@ -50,7 +50,7 @@ public class HitWhiteBall : MonoBehaviour
 
             ballMouseLine.SetPosition(0, whiteBall.transform.position);            
 
-            positionWhereMousePressed = worldCoordinatesMousePos;
+            positionWhereMousePressed = worldCoordinatesMousePos; 
         }        
 
         if (Input.GetMouseButtonUp(0)) 
@@ -58,7 +58,7 @@ public class HitWhiteBall : MonoBehaviour
             ballMouseLine.enabled = false;
             
             force = distanceMousePressedMouseReleased * forceMultiplier; //Calculamos la fuerza que va a resivir el rigid body
-
+            
             whiteBall.HitByCue(direction, force); //Le mandamos la direccion y la fuerza al rigid body
         }        
     }    
