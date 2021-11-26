adfbac3f78a85244456216d6bdf3bbf222bf0acb
Lobinux <lobinux@digilearnnials.com>
2021-11-26 09:25:06 -0300

    Re-pocicionamiento de la bola blanca

--- Assets/Script/BallContainer.cs
+++ Assets/Script/BallContainer.cs
@@ -2,9 +2,24 @@ using UnityEngine;
 
 public class BallContainer : MonoBehaviour
 {
+    [SerializeField] Vector3 startWhiteBallPosition = Vector3.zero;
     public void OnBallEnterTheHole(GameObject go)
     {
-        go.transform.SetParent(transform);
-        go.transform.localPosition = Vector3.zero;
+        PhysicBody physicBody = go.GetComponent<PhysicBody>();
+        
+        if(physicBody != null)
+        {
+            physicBody.SetAcceleration(0);
+        }
+
+        if (go.CompareTag("WhiteBall"))
+        {
+            go.transform.position = startWhiteBallPosition;
+        }
+        else
+        {
+            go.transform.SetParent(transform);
+            go.transform.localPosition = Vector3.zero;
+        }
     }
 }
